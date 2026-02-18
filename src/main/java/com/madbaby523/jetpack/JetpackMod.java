03cf670d46d429d57734b8292953f24a7b010db8
Madbaby523 <107885550+Madbaby523@users.noreply.github.com>
2026-02-18 16:38:27 -0600

    Create main mod class JetpackMod

---
+++ src/main/java/com/madbaby523/jetpack/JetpackMod.java
@@ -0,0 +1,8 @@
+package com.madbaby523.jetpack;
+
+public class JetpackMod {
+    public static void main(String[] args) {
+        // Main mod logic here
+        System.out.println("Jetpack Mod is running!");
+    }
+}
